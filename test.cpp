7c9d82d3678b8ca218881eeadef55bf15ee64454
Asad <asad.daqa.2006@gmail.com>
2025-04-07 20:28:56 +0300

    new stars

--- test.cpp
+++ test.cpp
@@ -3,5 +3,10 @@ using namespace std;
 #define ll long long
 #define nl '\n'
 int main(){
-    cout<<"Change 2"<<nl;
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5-i; ++j) {
+            cout<<" * ";
+        }
+        cout<<nl;
+    }
 }
