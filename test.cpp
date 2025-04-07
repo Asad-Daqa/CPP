a24e124bde8f04b641b84496964b989293b305a0
Asad <asad.daqa.2006@gmail.com>
2025-04-07 20:54:42 +0300

    a

--- test.cpp
+++ test.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main(){
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5-i; ++j) {
-            cout<<" * ";
+            cout<<"  *  ";
         }
+        cout<<nl;
         cout<<nl;
     }
 }
